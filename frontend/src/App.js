e2f71b26703badc258817eebdd350435a9010455
MethmaW <methma.cw@gmail.com>
2020-11-05 18:18:25 +0530

    before implementing react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import Rect from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import HomeScreen from "./screens/HomeScreen.js";
 
 const App = () => {
   return (
     <>
-     <h1>Welocme to proshop</h1>
+      <Header />
+      <main>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
