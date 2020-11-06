d7b1c24e19feca2bc068b0170c8d04f600649e34
MethmaW <methma.cw@gmail.com>
2020-11-06 13:28:04 +0530

    created product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import Rect from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import HomeScreen from "./screens/HomeScreen.js";
+import ProductScreen from "./screens/ProductScreen.js";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main>
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
